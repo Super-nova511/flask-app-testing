ef31a4a289877e5339407103011a9d2c5aa8e484
Super-nova511 <surekhakamble2112@gmail.com>
2025-12-01 18:19:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return send_file("New folder.zip", as_attachment=True)
+
+@app.route("/health")
+def download():
+    return "Working"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
